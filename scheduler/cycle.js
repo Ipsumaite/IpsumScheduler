9b708bd9c363d59c34ca1f6d9e465cbcd77e5f9d
derruba2000 <correia_ramos@hotmail.com>
2015-03-03 09:52:00 +0000

    Disabling salesforce

--- scheduler/cycle.js
+++ scheduler/cycle.js
@@ -7,7 +7,7 @@ var Firebase = require('firebase'),
 
 
 
-var myRootRef = new Firebase(process.env.FIREBASEURL+"/streams");
+var myChannelRef = new Firebase(process.env.FIREBASEURL+"/channels");
 var publishrate= process.env.PERIOD+"s";
 var myRef = new Firebase(process.env.FIREBASEURL+"/streams");
 
@@ -30,22 +30,37 @@ exports.running=function(){
                       beacon = beacons[key2];
                    }
                    var timediff = Math.round((Date.now()-beacon.date)/(1000*60));
-                   logger.logMessage("For user "+ key + "/"+ beacon.email+ " date difference in minutes :"+timediff);
+                   logger.logMessage("For user "+ key + "/"+ beacon.email+ " date difference since last activation report in minutes :"+timediff);
                      
-                   var strSOQL='SELECT Channel__c ,ContractNumber FROM Contract where AccountId = \'' + key + '\' AND SpecialTerms  not in (\'Canceled\')';
-                   sfWrapper.querySOQL(strSOQL, function (errorSubs, resultSubs) {
-                          if (errorSubs){
-                              logger.logError(' Not possible to look subscriptions for user ' + beacon.email);
-                          }else{
-                             logger.logMessage("Total Subscriptions " +resultSubs.totalSize + " found for user "+ beacon.email);
-                            for (var l=0; l< resultSubs.totalSize; l++){
-                              logger.logMessage("> Channel " + resultSubs.records[l].Channel__c + " subscribed by " + beacon.email);
-                              //Check published contents from each channel
+//                    var strSOQL='SELECT Channel__c ,ContractNumber FROM Contract where AccountId = \'' + key + '\' AND SpecialTerms  not in (\'Canceled\')';
+//                    sfWrapper.querySOQL(strSOQL, function (errorSubs, resultSubs) {
+//                           if (errorSubs){
+//                               logger.logError(' Not possible to look subscriptions for user ' + beacon.email);
+//                           }else{
+//                              logger.logMessage("Total Subscriptions " +resultSubs.totalSize + " found for user "+ key);
+//                             for (var l=0; l< resultSubs.totalSize; l++){
+//                               logger.logMessage("> Channel " + resultSubs.records[l].Channel__c + " subscribed by " + key);
+//                               //Check published contents from each channel
+//                               //For each channel what is the respective Account ID
+//                               var subchannel = resultSubs.records[l].Channel__c;
+//                               //for each accountID the reference for the published content
+//                               myChannelRef.once("value", function(snap){
+//                                 var accounts = snap.val();
+//                                 for (var acckey in accounts ){
+//                                   var channels = accounts[acckey];
+//                                   for (var chkey in channels){
+//                                     //console.log("\n##############"+subchannel + ".."+chkey);
+//                                     if ( subchannel == chkey)
+//                                       console.log("Account " + acckey + "- key " + chkey + "- Subscribed " +  subchannel);
+                                    
+//                                   }
+//                                 }
+//                                 //console.log(JSON.stringify(snap.val()));
+//                               });
                               
-                              
-                            }
-                          }
-                   });
+//                             }
+//                           }
+//                    });
 
                  });
 
